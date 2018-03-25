88a7c1de2a36ed84ca991b1f493d137b1bb158cd
huangyt39 <824551061@qq.com>
2018-03-26 00:16:40 +0800

    简答题提交

---
+++ Homework1/IMGUI/Assets/test.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    void Awake()
+    {
+        Debug.Log("onAwake");
+    }
+    void Start()
+    {
+        Debug.Log("onStart");
+    }
+
+    void Update()
+    {
+        Debug.Log("onUpdate");
+    }
+    void FixedUpdate()
+    {
+        Debug.Log("onFixedUpdate");
+    }
+    void LateUpdate()
+    {
+        Debug.Log("onLateUpdate");
+    }
+    void OnGUI()
+    {
+        Debug.Log("onGUI");
+    }
+    void Reset()
+    {
+        Debug.Log("onReset");
+    }
+    void OnDisable()
+    {
+        Debug.Log("onDisable");
+    }
+    void OnDestroy()
+    {
+        Debug.Log("onDestroy");
+    }
+}

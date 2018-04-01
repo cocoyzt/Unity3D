8246ccdb8b58d757f2d3f3d15855fd06dbb70058
huangyt39 <824551061@qq.com>
2018-04-01 09:28:18 +0800

    抛物线

---
+++ Homework2/Parabola/Assets/RigidBody.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidBody : MonoBehaviour {
+
+	public float vy = 15;
+	public float vx = 10;
+	public float t = 0;
+
+	// Use this for initialization
+	void Start () {
+		this.transform.position = new Vector3 (10, 0, -20);	
+		t = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		t = Time.deltaTime;
+		this.transform.position -= Vector3.right * t * vx;
+		this.transform.position += Vector3.up * t * vy;
+	}
+}
---
+++ Homework2/Parabola/Assets/Transfrom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transfrom : MonoBehaviour {
+
+	public float vy = 20;
+	public float vx = 10;
+	private float g = 9.8F;
+	public float t = 0;
+
+	// Use this for initialization
+	void Start () {
+		this.transform.position = new Vector3 (20, 0, 0);
+		t = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		t += Time.deltaTime;
+			this.transform.position = new Vector3 (20 - vx * t, vy * t - t * t * g, 0);
+	}
+}

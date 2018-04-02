ad43def2886fe382b324ea3b6ffaee0ba91bcbae
huangyt39 <824551061@qq.com>
2018-04-03 01:52:58 +0800

    完成魔鬼与牧师的基本逻辑和UI

---
+++ Homework2/Priests and Devils/Assets/On_Off.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class On_Off : MonoBehaviour {
+
+	private FirstSceneController firstSceneController;
+	enum Pos { On_BOAT, ON_SHONE};
+
+	// Use this for initialization
+	void Start () {
+		firstSceneController = (FirstSceneController)Director.getInstance().currentSceneControl;
+	}
+
+	private void OnMouseDown(){
+		if (firstSceneController.game_state == GameState.NOT_ENDED) {
+			if (firstSceneController.boat_state == FirstSceneController.BoatState.MOVING)
+				return;
+			int id = Convert.ToInt32 (this.name);
+			if(firstSceneController.boat_state == FirstSceneController.BoatState.STOPRIGHT){
+				for (int i = 0; i < 6; i++) {
+					if (firstSceneController.On_Shore_r [i] == id && firstSceneController.boat_capicity != 0) {
+						firstSceneController.On_Shore_r [i] = 6;
+						firstSceneController.boat_capicity--;
+						int Onto_Boat = 0;
+						for (int j = 0; j < 2; j++) {
+							if (firstSceneController.On_Boat [j] == 6) {
+								Onto_Boat = j;
+								break;
+							}
+						}
+						firstSceneController.On_Boat [Onto_Boat] = id;
+						float position_x = firstSceneController.Boat.transform.position.x - 2 + 4 * Onto_Boat;
+						this.transform.position = new Vector3 (position_x, 0.5f, 0);
+						return;
+					}
+				}
+
+				for (int i = 0; i < 2; i++) {
+					if (firstSceneController.On_Boat [i] == id) {
+						firstSceneController.On_Boat [i] = 6;
+						firstSceneController.boat_capicity ++;
+						int Onto_Shore = 0;
+						for (int j = 0; j < 6; j++) {
+							if (firstSceneController.On_Shore_r [j] == 6) {
+								Onto_Shore = j;
+								break;
+							}
+						}
+						firstSceneController.On_Shore_r [Onto_Shore] = id;
+						int position_x = 25 - Onto_Shore * 2;
+						this.transform.position = new Vector3 (position_x, 3, 0);
+						return;
+					}
+				}
+			}
+			else if(firstSceneController.boat_state == FirstSceneController.BoatState.STOPLEFT){
+				for (int i = 0; i < 6; i++) {
+					if (firstSceneController.On_Shore_l [i] == id && firstSceneController.boat_capicity != 0) {
+						firstSceneController.On_Shore_l [i] = 6;
+						firstSceneController.boat_capicity--;
+						int Onto_Boat = 0;
+						for (int j = 0; j < 2; j++) {
+							if (firstSceneController.On_Boat [j] == 6) {
+								Onto_Boat = j;
+								break;
+							}
+						}
+						firstSceneController.On_Boat [Onto_Boat] = id;
+						float position_x = firstSceneController.Boat.transform.position.x - 2 + 4 * Onto_Boat;
+						this.transform.position = new Vector3 (position_x, 0.5f, 0);
+						return;
+					}
+				}
+
+				for (int i = 0; i < 2; i++) {
+					if (firstSceneController.On_Boat [i] == id) {
+						firstSceneController.On_Boat [i] = 6;
+						firstSceneController.boat_capicity ++;
+						int Onto_Shore = 0;
+						for (int j = 0; j < 6; j++) {
+							if (firstSceneController.On_Shore_l [j] == 6) {
+								Onto_Shore = j;
+								break;
+							}
+						}
+						firstSceneController.On_Shore_l [Onto_Shore] = id;
+						int position_x = -25 + Onto_Shore * 2;
+						this.transform.position = new Vector3 (position_x, 3, 0);
+						return;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Homework2/Priests and Devils/Assets/UserGUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour {
+
+	private IUserAction action;
+
+	void Start(){
+		action = Director.getInstance ().currentSceneControl as IUserAction;
+	}
+
+	private void OnGUI(){
+		if (GUI.Button (new Rect (450, 100, 100, 40), "GO") && action.getGameState () == GameState.NOT_ENDED)
+			action.MoveBoat ();
+
+		if(action.getGameState() == GameState.WIN)
+			GUI.Label (new Rect (450, 50, 800, 450), "Win!");
+
+		if (action.getGameState () == GameState.FAILED)
+			action.GameOver ();
+	}
+}

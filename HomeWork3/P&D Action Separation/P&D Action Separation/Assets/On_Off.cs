086dc4ce33c1b941ae96f6b9f454899c88757d31
huangyt39 <824551061@qq.com>
2018-04-05 22:44:21 +0800

    添加动作分离器

---
+++ HomeWork3/P&D Action Separation/P&D Action Separation/Assets/On_Off.cs
@@ -0,0 +1,32 @@
+﻿/*  这个文件要挂载到所有的人物对象上，用来监听鼠标的点击事件，
+ *  对应的具体的动作在CCOn_OffAction.cs文件里面实现
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class On_Off : MonoBehaviour
+{
+
+	// Use this for initialization
+	private FirstSceneControl firstSceneControl;
+
+	void Start()
+	{
+		firstSceneControl = (FirstSceneControl)Director.getInstance().currentSceneControl;
+	}
+
+
+	private void OnMouseDown()
+	{
+		int id = Convert.ToInt32(this.name);
+		if (firstSceneControl.b_state != FirstSceneControl.BoatState.MOVING)
+		{
+			if (firstSceneControl.actionManager.on_off.ContainsKey(id)) firstSceneControl.actionManager.on_off[id].enable = true;
+			if (firstSceneControl.actionManager.on_off.ContainsKey(id - 6)) firstSceneControl.actionManager.on_off[id - 6].enable = true;
+		}
+
+	}    
+}
---
+++ HomeWork3/P&D Action Separation/P&D Action Separation/Assets/SSAction.cs
@@ -0,0 +1,27 @@
+﻿/* 这个文件是所有动作的基类
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SSAction : ScriptableObject {
+
+	public bool enable = false;
+	public bool destroy = false;
+
+	public GameObject gameobject { get; set; }
+	public Transform transform { get; set; }
+	public ISSActionCallback callback { get; set; }
+
+	protected SSAction() { }
+
+	public virtual void Start () {
+		throw new System.NotImplementedException();
+	}
+
+	// Update is called once per frame
+	public virtual void Update () {
+		throw new System.NotImplementedException();
+	}
+}

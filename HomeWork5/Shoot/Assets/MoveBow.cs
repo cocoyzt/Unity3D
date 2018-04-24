1255b645d26ab0036b8c0a7e56f5ec1df43fbcee
huangyt39 <824551061@qq.com>
2018-04-24 19:14:09 +0800

    完成射箭游戏的基本逻辑和UI

---
+++ HomeWork5/Shoot/Assets/MoveBow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBow : MonoBehaviour {
+
+	public float speedX = 2f;
+	public float speedY = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float translationY = Input.GetAxis ("Vertical") * speedY;
+		float translationX = Input.GetAxis ("Horizontal") * speedX;
+		translationX *= Time.deltaTime;
+		translationY *= Time.deltaTime;
+		transform.Translate (0, translationY, 0);
+		transform.Translate (0, 0, -translationX);
+		if (Input.GetButtonDown ("Fire1")) {
+			Debug.Log ("shoot!");
+			GameObject shootedArrow = Instantiate((GameObject)Resources.Load("Prefabs/arrow", typeof(GameObject)), this.transform.position, Quaternion.identity);
+			shootedArrow.name = "arrow";
+			shootedArrow.transform.localScale = new Vector3 (200f, 200f, 200f);
+			shootedArrow.transform.Rotate (0, -90, 90);
+			shootedArrow.AddComponent<Rigidbody> ();
+			shootedArrow.GetComponent<Rigidbody> ().AddForce(0, 0, 4000);
+			shootedArrow.GetComponent<Rigidbody> ().useGravity = true;
+			shootedArrow.AddComponent<BoxCollider>();
+			shootedArrow.GetComponent<BoxCollider> ().isTrigger = true;
+		}
+	}
+}
---
+++ HomeWork5/Shoot/Assets/UIController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+	private IUserAction action;
+
+	// Use this for initialization
+	void Start () {
+		action = Director.getInstance ().currentSceneController as IUserAction;
+	}
+	
+	void OnGUI(){
+		if (action.getGamestate () == GameState.INGAME) {
+			GUI.Label (new Rect (0, 0, 80, 40), "Score:" + action.getScore ().ToString ());
+			if(GUI.Button(new Rect(0, 50, 80, 40), "Pause")){
+				action.Pause ();
+			}
+			if(GUI.Button(new Rect(0, 100, 80, 40), "End")){
+				action.End ();
+			}
+		}
+		else if (action.getGamestate () == GameState.END) {
+			GUI.Label (new Rect (0, 0, 80, 40), "Score:" + action.getScore ().ToString ());
+			if(GUI.Button(new Rect(0, 50, 80, 40), "Start")){
+				action.Begin ();
+			}
+		}
+		else if (action.getGamestate () == GameState.PAUSE) {
+			GUI.Label (new Rect (0, 0, 80, 40), "Score:" + action.getScore ().ToString ());
+			if(GUI.Button(new Rect(0, 50, 80, 40), "Go on")){
+				action.Begin ();
+			}
+			if(GUI.Button(new Rect(0, 100, 80, 40), "End")){
+				action.End ();
+			}
+		}
+	}
+}

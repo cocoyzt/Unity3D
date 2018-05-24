ed1f88341a9769df7eb9e89a58687b72bfc41315
huangyt39 <824551061@qq.com>
2018-05-24 22:31:34 +0800

    Particle

---
+++ HomeWork7/ParticleSystem/Assets/Scripts/ParticleOnce.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleOnce : MonoBehaviour {
+
+    private ParticleSystem particleSys;  // 粒子系统  
+
+    // Use this for initialization
+    void Start () {
+        particleSys = this.GetComponent<ParticleSystem>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnEnable()
+    {
+        UI.OnclickAction += Boom;
+    }
+
+    private void OnDisable()
+    {
+        UI.OnclickAction -= Boom;
+    }
+
+    void Boom(Object sender, string info)
+    {
+        particleSys.Play();
+    }
+}
---
+++ HomeWork7/ParticleSystem/Assets/Scripts/UI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+
+    public delegate void ClickAction(Object sender, string info);
+    public static event ClickAction OnclickAction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(0, 0, 100, 30), "Boom!"))
+        {
+            if (OnclickAction != null) OnclickAction(this, "click1!");
+        }
+    }
+}

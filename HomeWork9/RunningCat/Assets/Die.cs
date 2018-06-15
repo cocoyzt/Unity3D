5b559a1a96efa91b7cb37ff9431a4fdce2a24469
huangyt39 <824551061@qq.com>
2018-06-15 17:32:18 +0800

    基本框架完成

---
+++ HomeWork9/RunningCat/Assets/Die.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Director.GetInstance().playing = false;
+        Debug.Log("Die!!");
+    }
+}

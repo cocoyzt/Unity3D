13ca6ed0ad4beeb546c0116941385ae09f900677
huangyt39 <824551061@qq.com>
2018-06-18 15:18:42 +0800

    track loop

--- HomeWork9/RunningCat/Assets/Scripts/TrackLoop.cs
+++ HomeWork9/RunningCat/Assets/Scripts/TrackLoop.cs
@@ -9,21 +9,25 @@ public class TrackLoop : MonoBehaviour {
     public GameObject track2;
     public GameObject runningTrack;
     public GameObject preparingTrack;
+    private float positionLf;
+    private float positionRg;
 
 	// Use this for initialization
 	void Start () {
         runningTrack = track1;
         preparingTrack = track2;
+        positionLf = runningTrack.transform.position.x;
+        positionRg = preparingTrack.transform.position.x;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        runningTrackx = runningTrack.transform.position.x;
 
-        if (runningTrack.transform.position.x <= -64f)
+        if (preparingTrack.transform.position.x <= positionLf)
         {
             Debug.Log("TrackLoop!!!");
-            runningTrack.transform.position = new Vector3(40.6f, runningTrack.transform.position.y, runningTrack.transform.position.z);
+            runningTrack.transform.position = new Vector3(positionRg, runningTrack.transform.position.y, runningTrack.transform.position.z);
+            preparingTrack.transform.position = new Vector3(positionLf, preparingTrack.transform.position.y, preparingTrack.transform.position.z);
             GameObject tmp = runningTrack;
             runningTrack = preparingTrack;
             preparingTrack = tmp;
